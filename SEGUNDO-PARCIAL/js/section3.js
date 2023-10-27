df90090e994b128309ba132c7ec903f120cb9f68
unknown <caroj9363mail.com>
2023-10-27 17:13:52 -0500

    terminado

--- SEGUNDO-PARCIAL/js/section3.js
+++ SEGUNDO-PARCIAL/js/section3.js
@@ -1,37 +1,24 @@
-
-function crearColaborador({ titulo, descripcion2 }) {
-    return `
-        <div class="colaborador">
-            <img src="img/imagen.webp">
-            <h1>${titulo}</h1>
-            <p>${descripcion2}</p>
-        </div>
-    `;
-}
-
 const data2 = [
-    { titulo: "Primer colaborador", descripcion2: "bla bla blaablabla" },
-    { titulo: "Segundo colaborador", descripcion2: "bla bla bl4aablabla" },
-    { titulo: "Tercer colaborador", descripcion2: "bla bla blaab43Sxlabla" }
+    { titulo: "Primer colaboradorohn Stith Pemberton", descripcion2: "un georgiano apasionado por las bebidas que invento la primera receta" },
+    { titulo: "pepsiman", descripcion2: "un gran competidor " },
+    { titulo: "recordar poner mas imagenes", descripcion2: "pruebaaaa" }
 ];
-
 function crearColaboradorConObjetos({ titulo, descripcion2 }) {
     const contenedorColaborador = document.createElement('div');
     const parrafo2 = document.createElement('p');
     parrafo2.innerText = descripcion2;
     const heading2 = document.createElement('h3');
     heading2.innerText = titulo;
-    const img = document.createElement('img');
-    img.src = "imagenes/logosuperior.png";
+    const img = document.createElement('img'); 
     contenedorColaborador.appendChild(img);
     contenedorColaborador.appendChild(heading2);
     contenedorColaborador.appendChild(parrafo2);
     contenedorColaborador.classList.add("colaborador");
+    img.src ="imagenes/icono.png"; 
     return contenedorColaborador;
+    
 }
-
 const contenedor2 = document.querySelector('.contenedor-Colaboradores');
-
 for (colaborador of data2) {
     const colaboradorHtml = crearColaboradorConObjetos(colaborador);
     contenedor2.appendChild(colaboradorHtml);
